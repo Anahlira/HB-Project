2153890e628a2bc659c19ddb308b6d9c093bd3ed
Teodora Slavova <morska_kotka1@abv.bg>
2023-10-28 20:19:10 +0300

    Basic set-up

---
+++ src/Components/Authentication.js
@@ -0,0 +1,11 @@
+import "./Authentication.css";
+
+function Authentication() {
+  return (
+    <div class="titleBg">
+      <h1>Home Bank Project</h1>;
+    </div>
+  );
+}
+
+export default Authentication;
