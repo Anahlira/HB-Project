7436393c761a7f41b52f24345f81736df4f8e31a
Teodora Slavova <morska_kotka1@abv.bg>
2023-11-03 21:07:23 +0200

    Formik set-up + fixes

--- src/Components/Authentication/InputComponent/Input.tsx
+++ src/Components/Authentication/InputComponent/Input.tsx
@@ -1,12 +1,17 @@
 import "./Input.css";
 import IconProfile from "../Icon-svg.js/IconProfile";
 import IconLock from "../Icon-svg.js/IconLock";
+import { useEffect, useState } from "react";
 
 interface PropsI {
   type: "email" | "password";
+  formik: any;
 }
 
 const Input = (props: PropsI) => {
+  const [changed, setChanged] = useState(false);
+  const [err, setErr] = useState(false);
+
   const labelText = {
     email: "Email Address",
     password: "Password",
@@ -17,19 +22,41 @@ const Input = (props: PropsI) => {
     password: <IconLock />,
   }[props.type];
 
-  return (
-    <div className="field">
-      {icon}
+  const value = {
+    email: props.formik.values.email,
+    password: props.formik.values.password,
+  }[props.type];
 
-      <input
-        id={props.type}
-        name={props.type}
-        type={props.type}
-        // onChange={formik.handleChange}
-        // onBlur={formik.handleBlur}
-        // value={formik.values.email}
-      />
-      <label htmlFor="email">{labelText}</label>
+  useEffect(() => {
+    if (value === props.formik.initialValues[props.type]) {
+      setChanged(false);
+    } else setChanged(true);
+
+    setErr(props.formik.errors[props.type] && props.formik.touched[props.type]);
+  }, [value]);
+
+  return (
+    <div className="inputBox">
+      <div className={`field ${err ? "errorBorder" : ""}`}>
+        {icon}
+
+        <input
+          id={props.type}
+          name={props.type}
+          type={props.type}
+          onChange={props.formik.handleChange}
+          onBlur={props.formik.handleBlur}
+          value={value}
+        />
+        <label
+          className={changed ? "labelPlaceholder" : "labelUp"}
+          htmlFor="email"
+        >
+          {labelText}
+        </label>
+      </div>
+
+      <p>{err && props.formik.errors[props.type]}</p>
     </div>
   );
 };
--- src/Components/Authentication/Login.tsx
+++ src/Components/Authentication/Login.tsx
@@ -1,12 +1,43 @@
+import { useFormik } from "formik";
 import Input from "./InputComponent/Input";
+import * as Yup from "yup";
+import Button from "./ButtonComponet/Button";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(4, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
 const Login = () => {
+  const handleLogin = (values: { email: string; password: string }) => {
+    console.log(values.email);
+    console.log(values.password);
+    console.log("Login!! Almost");
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: handleLogin,
+    validationSchema: LoginSchema,
+  });
+
   return (
     <>
-      <form className="authForm">
-        <Input type="email"></Input>
-        <Input type="password"></Input>
+      <form className="authForm" onSubmit={formik.handleSubmit}>
+        <Input key="1" type="email" formik={formik}></Input>
+        <Input key="2" type="password" formik={formik}></Input>
+        {/* <button type="submit" disabled={formik.isSubmitting}>
+          Submit
+        </button> */}
+        <Button />
       </form>
+      <div>{formik.touched.password}</div>
     </>
   );
 };
